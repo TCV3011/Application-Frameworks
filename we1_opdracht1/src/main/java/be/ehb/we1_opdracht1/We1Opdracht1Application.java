914656a959ae29eed7084bf4a7529a560c3c1b5e
TCV3011 <tycho.verstraete@student.ehb.be>
2020-09-24 15:48:00 +0200

    creating project

---
+++ we1_opdracht1/src/main/java/be/ehb/we1_opdracht1/We1Opdracht1Application.java
@@ -0,0 +1,13 @@
+package be.ehb.we1_opdracht1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class We1Opdracht1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(We1Opdracht1Application.class, args);
+    }
+
+}
---
+++ we1_opdracht1/src/test/java/be/ehb/we1_opdracht1/We1Opdracht1ApplicationTests.java
@@ -0,0 +1,13 @@
+package be.ehb.we1_opdracht1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class We1Opdracht1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
