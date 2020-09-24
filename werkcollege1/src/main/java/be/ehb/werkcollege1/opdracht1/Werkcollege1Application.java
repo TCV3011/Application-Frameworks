2ea55e75766f0e29447a848a0947fcd21b648cad
TCV3011 <tycho.verstraete@student.ehb.be>
2020-09-24 15:58:28 +0200

    creating right project

---
+++ werkcollege1/src/main/java/be/ehb/werkcollege1/opdracht1/Werkcollege1Application.java
@@ -0,0 +1,13 @@
+package be.ehb.werkcollege1.opdracht1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Werkcollege1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Werkcollege1Application.class, args);
+    }
+
+}
---
+++ werkcollege1/src/test/java/be/ehb/werkcollege1/Werkcollege1ApplicationTests.java
@@ -0,0 +1,13 @@
+package be.ehb.werkcollege1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Werkcollege1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
