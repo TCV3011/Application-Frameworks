356e17b3e6f0566d71f8698f70365bb11cfbcbf6
TCV3011 <tycho.verstraete@student.ehb.be>
2020-09-24 17:05:24 +0200

    added challenges
    search by id does not work yet

--- werkcollege1/src/main/java/be/ehb/werkcollege1/opdracht3/controllers/ThirdController.java
+++ werkcollege1/src/main/java/be/ehb/werkcollege1/opdracht3/controllers/ThirdController.java
@@ -3,9 +3,7 @@ package be.ehb.werkcollege1.opdracht3.controllers;
 import be.ehb.werkcollege1.opdracht3.models.DataSource;
 import be.ehb.werkcollege1.opdracht3.models.Evenement;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 
@@ -14,7 +12,23 @@ public class ThirdController {
 
     @RequestMapping(value = "/evenementen", method = RequestMethod.GET)
     @ResponseBody
-    public ArrayList<Evenement> evenements() {
+    public ArrayList<Evenement> getEvenements() {
         return  DataSource.getEvenements();
+    }
+
+    // challenge 1
+    @RequestMapping(value = "/evenement/{id}", method = RequestMethod.GET)
+    @ResponseBody
+    public Evenement getEvenementById(@PathVariable(value = "id") int id) {
+        return DataSource.getEvenementById(id);
+    }
+
+    //challenge 2
+    @RequestMapping(value = "/addEvenement", method = RequestMethod.POST)
+    @ResponseBody
+    public  Evenement addEvenement(@RequestParam(name = "id") int id, @RequestParam(name = "naam") String naam, @RequestParam(name = "soort") String soort) {
+        Evenement temp = new Evenement(id, naam, soort);
+        DataSource.addEvenement(temp);
+        return temp;
     }
 }
